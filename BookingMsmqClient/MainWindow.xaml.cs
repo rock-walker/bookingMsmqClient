68561a5a2525d13d99dbeeb6348b18832018f289
Andrei Melnikau <melnikov.work@gmail.com>
2016-02-09 00:15:02 +0300

    started

---
+++ BookingMsmqClient/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BookingMsmqClient.Models;
+
+namespace BookingMsmqClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Seat[,] _room = new Seat[30,40];
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitSpace();
+        }
+
+        private void InitSpace()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    var seat = GetEmptySeat();
+                    canvas.Children.Add(seat);
+                    Canvas.SetTop(seat, i * 10);
+                    Canvas.SetLeft(seat, j * 10);
+                }
+            }
+
+        }
+
+        private Rectangle GetEmptySeat()
+        {
+            var rect = new Rectangle
+            {
+                Width = 10,
+                Height = 10,
+                Fill = new SolidColorBrush(Colors.White),
+                RadiusX = 1.5,
+                RadiusY = 1.5,
+                Stroke = new SolidColorBrush(Colors.Black)
+            };
+
+            rect.MouseDown += DetectSeat;
+
+            return rect;
+        }
+
+        private void DetectSeat(object sender, MouseButtonEventArgs args)
+        {
+            
+        }
+    }
+}
---
+++ BookingMsmqClient/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BookingMsmqClient.Models
+{
+    class Customer
+    {
+        public Guid OrderId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
