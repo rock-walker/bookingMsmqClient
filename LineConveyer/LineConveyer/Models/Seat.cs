fb7c565eb6a4cdc446e2a6b29b93e05d95a9879f
Andrey Melnikov <melnikov.work@gmail.com>
2016-02-18 16:42:10 +0300

    added Console for messages

---
+++ LineConveyer/LineConveyer/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace BookingMsmqClient.Models
+{
+    public class Seat
+    {
+        public int Number { get; set; }
+        public int Row { get; set; }
+        public BookingState BookingState { get; set; }
+        public Customer Data { get; set; }
+        [XmlIgnore]
+        public Rectangle ViewModel { get; set; }
+    }
+}
---
+++ LineConveyer/LineConveyer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Messaging;
+
+namespace BookingMsmqClient
+{
+    class Program
+    {
+        private readonly string _queueName = ".\\Private$\\SeatsRoom";
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        private void ReadQueue()
+        {
+            using (var queue = new MessageQueue(_queueName))
+            {
+                var enumerator = queue.GetMessageEnumerator2();
+
+                while (enumerator.MoveNext(TimeSpan.FromMilliseconds(10000)) && enumerator.Current != null)
+                {
+                    var item = enumerator.Current;
+                    item.Body as Seat;
+                }
+
+            }
+        }
+    }
+}
